965c0b092a05885e5cdbb922c75b4ea04924c479
Milashkarina <166594541+Milashkarina@users.noreply.github.com>
2024-10-29 01:43:36 +0300

    Add_modals_window

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,17 @@
-var button = document.getElementById("myButton");
+//Модальное окно на странице "Контакты"
+var button2 = document.getElementById("myButton");
 var modal = document.getElementById("myModal");
 var close = document.getElementById("myClose");
 
 
-button.onclick = function() {
+button2.onclick = function() {
     modal.style.display = "block";
 }
 
 close.onclick = function() {
     modal.style.display = "none";
 }
+
+
+
+
---
+++ js/modals_image.js
@@ -0,0 +1,49 @@
+var modal1 = document.getElementById("myModal1");
+var dos_1 = document.getElementById("myImg1");
+var close1 = document.getElementById("myClose1");
+
+dos_1 .onclick = function() {
+    modal1.style.display = "block";
+}
+
+close1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+
+var modal2 = document.getElementById("myModal2");
+var dos_2 = document.getElementById("myImg2");
+var close2 = document.getElementById("myClose2");
+
+dos_2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+close2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+var modal3 = document.getElementById("myModal3");
+var dos_3 = document.getElementById("myImg3");
+var close3 = document.getElementById("myClose3");
+
+dos_3.onclick = function() {
+    modal3.style.display = "block";
+}
+
+close3.onclick = function() {
+    modal3.style.display = "none";
+}
+
+var modal4 = document.getElementById("myModal4");
+var dos_4 = document.getElementById("myImg4");
+var close4 = document.getElementById("myClose4");
+
+dos_4.onclick = function() {
+    modal4.style.display = "block";
+}
+
+close4.onclick = function() {
+    modal4.style.display = "none";
+}
+
